b4895a5f9976d9ee8d4fe7c5fbe9f4eb48feafb3
Lovinder Pnag <lspnag@gmail.com>
2016-01-31 11:08:26 -0500

    test for valid schedule;

--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -4,10 +4,9 @@ describe 'BabySitterPay' do
   before(:each) do
     @sitting4 = BabySitterPay.new(17,22,24)
     @sitting5 = BabySitterPay.new(17,22,25)
-    @sitting6 = BabySitterPay.new(17,22,28)
   end
 
-  context 'invalid schedule' do
+  context 'invalid or valid schedule?' do
     it 'start time before 5pm results in invalid schedule' do
       @sitting = BabySitterPay.new(16,22,28)
       expect(@sitting.message).to eq "Must have start time after 5pm"
@@ -17,6 +16,11 @@ describe 'BabySitterPay' do
       @sitting1 = BabySitterPay.new(17,22,29)
       expect(@sitting1.message).to eq "Must have end time before 4am"
     end
+
+    it 'valid schedule has proper message' do
+      @sitting2 = BabySitterPay.new(17,22,22)
+      expect(@sitting2.message).to eq "You have a valid schedule!"
+    end
   end
 
   context 'calculating pay for various pay hours' do
@@ -25,14 +29,18 @@ describe 'BabySitterPay' do
       expect(@sitting2.calculate_pay).to eq 60
     end
 
-    it 'pay for hours with hours after bedtime' do
+    it 'pay for hours after bedtime' do
       @sitting3 = BabySitterPay.new(17,22,23)
       expect(@sitting3.calculate_pay).to eq 68
     end
+
+    it 'pay for hours after midnight' do
+      @sitting6 = BabySitterPay.new(17,22,28)
+      expect(@sitting6.calculate_pay).to eq 140
+    end
   end
 end
-# p @sitting2.calculate_pay == 60
-# p @sitting3.calculate_pay == 68
+
 # p @sitting4.calculate_pay == 76
 # p @sitting5.calculate_pay == 92
 # p @sitting6.calculate_pay == 140
